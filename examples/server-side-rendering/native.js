7c4313030f92cfdbba09823d7d2d0837bd26f60e
marcmrf <marc.garcia@marfeel.com>
2020-05-19 10:42:31 +0200

    better example

--- examples/server-side-rendering/native.js
+++ examples/server-side-rendering/native.js
@@ -1,16 +1,14 @@
 import React, { useState } from 'react'
 
 export default () => {
-	const [counter, setCounter] = useState(0);
+  const [counter, setCounter] = useState(0);
 
-	return (
-		<div>
-			<button onClick={() => console.log('click')}>press me</button>
-			<div onClick={() => setCounter(counter + 1)}>
-				click me too! {counter}
-			</div>
-		</div>
-	);
+  return (
+    <div>
+      <button type="button" onClick={() => setCounter(counter + 1)}>
+        click me
+        {counter}
+      </button>
+    </div>
+  );
 };
-
-export const pepe = 'lelelel';
